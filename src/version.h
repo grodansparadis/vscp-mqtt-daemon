cfa9d7c7bc10f7cbc80cd9932a8f9698b380cc41
Ake Hedman <akhe@grodansparadis.com>
2026-08-18 17:20:28 +0200

    CI now building artifacts

---
+++ src/version.h
@@ -0,0 +1,15 @@
+#ifndef VSCPD_VERSION_H
+#define VSCPD_VERSION_H
+
+#define VSCPD_VERSION_MAJOR 1
+#define VSCPD_VERSION_MINOR 0
+#define VSCPD_VERSION_PATCH 0
+
+#define VSCPD_VERSION_STRING "1.0.0"
+#define VSCPD_DISPLAY_VERSION "1.0.0"
+
+#define VSCPD_COPYRIGHT \
+	"Copyright (C) 2000-2026 Ake Hedman, the VSCP project, " \
+	"https://www.vscp.org"
+
+#endif
